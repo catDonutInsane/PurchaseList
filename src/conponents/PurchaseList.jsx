80c279926b8a35dc00e86f844e50300458fc054e
catDonutInsane <124159300+catDonutInsane@users.noreply.github.com>
2023-04-30 14:50:07 +0300

    g

--- src/conponents/PurchaseList.jsx
+++ src/conponents/PurchaseList.jsx
@@ -4,19 +4,27 @@ import style from "./PurchaseList.module.css"
 
 export const PurchaseList = (props) =>{
     const [name, setName] = useState('')
-    const [price, setPrice] = useState('')
+    const [price, setPrice] = useState("")
     const [amount, setAmount] = useState(1)
     return(
-        <div>
+        <div className={style.mainWrap}>
             <div className={style.addPurchase}>
                 <input onChange={(e)=>setName(e.currentTarget.value)} value={name} placeholder="Название"></input>
                 <input onChange={(e)=>setPrice(e.currentTarget.value)} value={price} placeholder="Цена"></input>
                 <input onChange={(e)=>setAmount(e.currentTarget.value)} value={amount} type="text" />
-                <button onClick={()=>{props.addTask(name, price,amount)
+                
+            </div>
+            <button onClick={()=>{
+                if(!price){ return}
+                else{
+                    props.addTask(name, price,amount)
+                        
                         setName("")
                         setPrice("")
+                }
+                
+                        
                 }}>Добавить</button>
-            </div>
         </div>
     )
 }
