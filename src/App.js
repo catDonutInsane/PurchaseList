20c4c2aea8919eae6c7da757b57ec3d233ae90c0
catDonutInsane <124159300+catDonutInsane@users.noreply.github.com>
2023-04-30 20:18:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import {Input} from "./conponents/Input"
 import {ListItem} from "./conponents/ListItem"
 
 function App() {
-  const [list, setList] = useState([])
+  const [list, setList] = useState(JSON.parse(localStorage.getItem('List'))||[])
+  
   let addTask = (name, price,amount)=>{
     if(isNaN(price)) return
+    
     setList([...list,{name, price:price*amount, amount}])
+    
+     
   }
+  useEffect(()=>{
+    localStorage.setItem("List",JSON.stringify(list))
+    
+  },[list])
+
   let totalPrice = list.map(i=>i.price)
                 .reduce((prev,next)=>+prev+ +next,0)
-                console.log(totalPrice);
   return (
     <div className='wrapper'>
       <p>Укажите сумму,которую готовы потратить </p>
-      <Input addTask={addTask} list={list} totalPrice={totalPrice}/>
+      <Input addTask={addTask}  totalPrice={totalPrice}/>
       {list.map( item =>{
     return <ListItem item={item}/>
   })}
