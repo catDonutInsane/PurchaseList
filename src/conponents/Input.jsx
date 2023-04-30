ec2a4ef5b77bd11e6ffa9e35377d127f193b0105
catDonutInsane <124159300+catDonutInsane@users.noreply.github.com>
2023-04-30 13:47:09 +0300

    ui

--- src/conponents/Input.jsx
+++ src/conponents/Input.jsx
@@ -1,52 +1,60 @@
 import React from "react";
 import { useState } from "react";
-import style from "./Input.module.css"
-import {PurchaseList} from "./PurchaseList"
-export const Input=(props)=>{
-    const [value, setValue] = useState("")
-    const [money, setMoney] = useState(0)
-    let clickBtn =(e)=>{
-        if(isNaN(value)|| value===""){
-            setValue(value)
-            setMoney("Введите число")
-            e.currentTarget.value = ""
-            
-        }else{
-            setMoney(value)
-            e.currentTarget.value = ""
-        }
-    }
-    const pressEnter=(e)=>{
-        if(e.key==="Enter"){
-            clickBtn(e)
-            
-          
-        }
-    }
-    
-    return(
-        <div className={style.main}>
-        <div className={style.wrapper}>
+import style from "./Input.module.css";
+import { PurchaseList } from "./PurchaseList";
 
-            <div className={style.inpbnt}>
-            <input 
-            onChange={(e)=>{
-                setValue(e.currentTarget.value)
-                }}
+export const Input = (props) => {
+  const [value, setValue] = useState("");
+  const [money, setMoney] = useState(0);
+  
+  let clickBtn = (e) => {
+    if (isNaN(value) || value === "") {
+      setValue(value);
+      setMoney("Введите число");
+      e.currentTarget.value = "";
+    } else {
+      setMoney(value);
+      e.currentTarget.value = "";
+    }
+  };
+  const pressEnter = (e) => {
+    if (e.key === "Enter") {
+      clickBtn(e);
+    }
+  };
+
+  return (
+    <div className={style.main}>
+      <div className={style.wrapper}>
+        <div className={style.inpbnt}>
+          <input
+            onChange={(e) => {
+              setValue(e.currentTarget.value);
+            }}
             onKeyDown={pressEnter}
-                    value={value} 
-                    className={style.inputSum} 
-                    type="text" 
-                    placeholder="введите сумму"></input>
-                <button onClick={clickBtn}><span>ok</span></button>
-            </div>
+            value={value}
+            className={style.inputSum}
+            type="text"
+            placeholder="введите сумму"
+          ></input>
+          <button onClick={clickBtn}>
+            <span>ok</span>
+          </button>
+        </div>
 
-                    <div>
-                        <h1>вы готовы проебать: {isNaN(value)?"Введите численное значение":props.totalPrice?money-props.totalPrice:money} рублей</h1>
-                    </div>
-                    
+        <div>
+          <h1>
+            вы готовы проебать:{" "}
+            {isNaN(value)
+              ? "Введите численное значение"
+              : props.totalPrice
+              ? money - props.totalPrice
+              : money}{" "}
+            рублей
+          </h1>
         </div>
-        <PurchaseList addTask={props.addTask}/>
-        </div>
-    )
-}
+      </div>
+      <PurchaseList addTask={props.addTask} />
+    </div>
+  );
+};
