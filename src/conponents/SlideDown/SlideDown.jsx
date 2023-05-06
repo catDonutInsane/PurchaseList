218e36a6bf092976346dbf042d83fea12622e9e6
catDonutInsane <124159300+catDonutInsane@users.noreply.github.com>
2023-05-06 19:53:12 +0300

    g

--- src/conponents/SlideDown/SlideDown.jsx
+++ src/conponents/SlideDown/SlideDown.jsx
@@ -5,11 +5,13 @@ export  const SlideDown=()=>{
 
   let mainBl = useRef();
   
-  let chBlocksCoord = (count=0)=>{
+  let chBlocksCoord = (count=0,delay=0.6)=>{
    
     let block = Array.from(mainBl.current.children)
     for(let i=0; i<block.length; i++){
       block[i].style.top=100+count+"px"
+      block[i].style.transition=`1s ease ${delay}s` ;
+      delay+=0.3
       count+=80
     }
   }
@@ -34,6 +36,7 @@ export  const SlideDown=()=>{
              <div ref={mainBl}>
              <div  className={style.blocks} >HUI</div>
              <div  className={style.blocks} >PISKA</div>
+             <div  className={style.blocks} >GOVNO</div>
              </div>
              
              </div>
