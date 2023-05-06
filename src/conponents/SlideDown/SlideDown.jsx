132895c7058f0c219435878c49b4ca5d7ee4b5cc
catDonutInsane <124159300+catDonutInsane@users.noreply.github.com>
2023-05-06 17:55:57 +0300

    d

--- src/conponents/SlideDown/SlideDown.jsx
+++ src/conponents/SlideDown/SlideDown.jsx
@@ -1,19 +1,31 @@
 import React,{useRef} from "react";
 import style from "./SlideDown.module.css"
 
-export const SlideDown=()=>{
+export  const SlideDown=()=>{
+  let bl = useRef()
+  let bl1 = useRef()
     let slDOWN = useRef()
     const slideDown=()=>{
         slDOWN.current.style.top=0
+        bl.current.style.top=100+"px"
+        bl1.current.style.top=190+"px"
       }
-      const slideUp=()=>{
+    const slideUp=()=>{
+      bl.current.style.top=-500+"px"
+      bl1.current.style.top=-500+"px"
         slDOWN.current.style.top=-1000+"px"
       }
+
     return(
         <div className={style.wrapper}>
+          
              <div ref={slDOWN} className={style.slideDown}>
-             <button onClick={slideUp}>Скрыть</button>
+             
+             <button onClick={
+                          slideUp}>Скрыть</button>
              пока здесь ничего нет
+             <div ref={bl} className={style.blocks} ></div>
+             <div ref={bl1} className={style.blocks} ></div>
              </div>
         <button onClick={slideDown}>Меню</button>
         </div>
